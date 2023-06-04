412e942b6fa7ebf84e3c23306d3fdc62f819191c
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-06-04 14:15:10 +0400

    создание матрицы, два метода:размер и получение элемента по индексу

--- Source.cpp
+++ Source.cpp
@@ -17,3 +17,53 @@ protected:
 	Matrix() = default;
 };
 
+class Matrixs : public Matrix {
+	vector <double> _matrix;
+	int _size;
+
+public:
+	Matrixs() : _size(5)
+	{
+		_matrix.push_back(2);
+		_matrix.push_back(4);
+		_matrix.push_back(7);
+		_matrix.push_back(2);
+		_matrix.push_back(0);
+		_matrix.push_back(8);
+		_matrix.push_back(11);
+		_matrix.push_back(4);
+		_matrix.push_back(5);
+		_matrix.push_back(6);
+		_matrix.push_back(2);
+		_matrix.push_back(24);
+		_matrix.push_back(1);
+		_matrix.push_back(2);
+		_matrix.push_back(7);
+		_matrix.push_back(9);
+		_matrix.push_back(3);
+		_matrix.push_back(2);
+		_matrix.push_back(7);
+		_matrix.push_back(3);
+		_matrix.push_back(11);
+		_matrix.push_back(3);
+		_matrix.push_back(5);
+		_matrix.push_back(4);
+		_matrix.push_back(2);
+
+	}
+
+	int size() const override {
+		return _size;
+	};
+	double get(int i, int j) const override {
+		return _matrix[i * _size + j];
+	};
+
+
+};
+
+int main()
+{
+	Matrixs matrix;
+	cout << matrix.get(2, 3) << endl;
+}
